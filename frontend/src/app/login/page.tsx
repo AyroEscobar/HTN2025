5e36cd443182b60f949da3252dc176ba2694c7e9
AlanRoybal <alanroybal05@gmail.com>
2025-09-13 12:55:20 -0400

    login done

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
+import Link from "next/link";
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState("");
@@ -22,8 +23,13 @@ const Login: React.FC = () => {
   };
 
   return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col justify-center items-center w-1/3 p-4">
     <form onSubmit={handleLogin}>
-      <h2>Login</h2>
+      <div className="mb-8">
+        <h3>Log into TypeChuzz</h3>
+      <h1 className="pt-4">Email</h1>
+        <div className="rounded-md border border-gray-300 p-2 bg-input">
       <input
         type="email"
         placeholder="Email"
@@ -31,6 +37,9 @@ const Login: React.FC = () => {
         onChange={e => setEmail(e.target.value)}
         required
       />
+      </div>
+      <h1 className="pt-4">Password</h1>
+        <div className="rounded-md border border-gray-300 p-2 bg-input">
       <input
         type="password"
         placeholder="Password"
@@ -38,9 +47,20 @@ const Login: React.FC = () => {
         onChange={e => setPassword(e.target.value)}
         required
       />
-      <button type="submit">Login</button>
+      </div>
+      </div>
+      <div>
+      <button type="submit" className="bg-accent px-4 py-2 rounded-md w-full mt-4 hover:bg-secondary text-white">Login</button>
       {error && <p style={{color: "red"}}>{error}</p>}
+      </div>
     </form>
+
+    <div className="mt-4">Don't have an account? Click <span><Link href="/register" passHref><button className="text-accent hover:underline">here</button></Link></span>.</div>
+    </div>
+    <div className="w-2/3 flex items-center justify-center bg-bglight">
+      <img src="/register.png" alt="Register Illustration" className="object-contain h-full w-full" />
+    </div>
+    </div>
   );
 };
 
